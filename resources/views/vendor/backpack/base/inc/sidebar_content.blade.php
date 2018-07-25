22313514654b39a80bbeb0482788baca4131b678
Idist <minhtoan96.it@gmail.com>
2018-07-25 18:23:59 +0700

    tạo ACL và quản lý menu

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -12,6 +12,21 @@
     </a>
 </li>
 <li>
+    <a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/menu-item') }}">
+        <i class="fa fa-list"></i>
+        <span>Menu</span>
+    </a>
+</li>
+<!-- Users, Roles Permissions -->
+<li class="treeview">
+    <a href="#"><i class="fa fa-group"></i> <span>Users, Roles, Permissions</span> <i class="fa fa-angle-left pull-right"></i></a>
+    <ul class="treeview-menu">
+        <li><a href="{{ backpack_url('user') }}"><i class="fa fa-user"></i> <span>Users</span></a></li>
+        <li><a href="{{ backpack_url('role') }}"><i class="fa fa-group"></i> <span>Roles</span></a></li>
+        <li><a href="{{ backpack_url('permission') }}"><i class="fa fa-key"></i> <span>Permissions</span></a></li>
+    </ul>
+</li>
+<li>
     <a href="{{ backpack_url('elfinder') }}">
         <i class="fa fa-files-o"></i>
         <span>{{ trans('backpack::crud.file_manager') }}</span>
