b3c30c346173f420f29ab754ad32499d58943de6
Idist <minhtoan96.it@gmail.com>
2018-07-25 09:44:34 +0700

    Khởi tạo nền backpack

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,19 @@
+@extends("errors.error_layout")
+@section("error_title")
+    {{ trans('errors.405.title') }}
+@endsection
+@section("error_detail")
+    <div class="content">
+        <div class="title">{{ trans('errors.405.code') }}</div>
+        <div class="quote">{{ trans('errors.405.error_title') }}</div>
+        <div class="explanation">
+            <br>
+            <small>
+                @php
+                    $default_error_message = trans('errors.405.message');
+                @endphp
+                {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
+            </small>
+        </div>
+    </div>
+@endsection
